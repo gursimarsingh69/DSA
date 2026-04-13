ebed14052c26cf44bc790229c01f47637b021e03
Gursimar Singh <gursimar1731@gmail.com>
2026-04-13 17:37:57 +0530

    Dutch National Flag algo

--- Arrays/75-SortColors.cpp
+++ Arrays/75-SortColors.cpp
@@ -5,9 +5,16 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]) swap(nums[j],nums[j+1]);
+        int low=0;int mid=0;int high=n-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low],nums[mid]); low++ ; mid++;
+            } 
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[high],nums[mid]); high--;
             }
         }
     }
