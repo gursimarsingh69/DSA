b51e348406ea5061a722b9b56764e0135ff9f81b
Gursimar Singh <gursimar1731@gmail.com>
2026-04-15 18:42:53 +0530

    hash map

--- Arrays/560-LongestSubarrayofSumK.cpp
+++ Arrays/560-LongestSubarrayofSumK.cpp
@@ -1,25 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-    int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        int left = 0;
-        int right = 0;
-        long int sum = 0;
-        int maxLength = 0;
-        while (right < n) {
-            sum += nums[right];
-            while (left <= right && sum > k) {
-                sum -= nums[left];
-                left++;
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> mp; 
+        for(int i=0; i<nums.size(); i++) {
+            int complement = target - nums[i];
+            if(mp.find(complement) != mp.end()) {
+                return {mp[complement], i};
             }
-            if (sum == k) {
-                maxLength = max(maxLength, right - left + 1);
-            }
-            right++;
+            mp[nums[i]] = i;
         }
-        return maxLength;
+        return {};
     }
 };
