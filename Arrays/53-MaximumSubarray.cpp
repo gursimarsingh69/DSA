9570f1afec9b5a90ddd33996e7c58f4898d82c49
Gursimar Singh <gursimar1731@gmail.com>
2026-07-12 18:37:50 +0530

    kadane's algo

--- Arrays/53-MaximumSubarray.cpp
+++ Arrays/53-MaximumSubarray.cpp
@@ -4,15 +4,12 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int maxNum= INT_MIN;
-        for(int i=0;i<n;i++){
-            int sum=0;
-            for(int j=0;j<n;j++){
-                sum+=nums[j];
-                maxNum=max(sum,maxNum);
-            }
+        int maxSoFar = nums[0];
+        int currentMax = nums[0];
+        for (size_t i = 1; i < nums.size(); i++) {
+            currentMax = max(nums[i], currentMax + nums[i]);
+            maxSoFar = max(maxSoFar, currentMax);
         }
-        return maxNum;
+        return maxSoFar;
     }
 };
