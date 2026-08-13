d4176db2d4f409265e4b5960c8475dd20ecbe1c2
Gursimar Singh <gursimar1731@gmail.com>
2026-08-13 13:45:42 +0530

    extended boyer moore voting algo

--- Arrays/229-MajorityElement.cpp
+++ Arrays/229-MajorityElement.cpp
@@ -4,13 +4,36 @@ using namespace std;
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int,int>hasht;    
-        for(int i:nums) hasht[i]++;
-        vector<int> result;
-        int threshold = nums.size()/3;
-        for(auto &[i,j] : hasht){
-            if(j>threshold) result.push_back(i);
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = 0, el2 = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (cnt1 > 0 && nums[i] == el1) {
+                cnt1++;
+            } else if (cnt2 > 0 && nums[i] == el2) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                el1 = nums[i];
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                el2 = nums[i];
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
         }
+        cnt1 = 0;
+        cnt2 = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == el1) cnt1++;
+            else if (nums[i] == el2) cnt2++;
+        }
+
+        vector<int> result;
+        if (cnt1 > nums.size() / 3) result.push_back(el1);
+        if (cnt2 > nums.size() / 3) result.push_back(el2);
+
         return result;
     }
 };
