e19c65658187f1d3bad00a4ccaf8b71f68c42919
Gursimar Singh <gursimar1731@gmail.com>
2026-08-15 22:03:10 +0530

    merge sort

--- Arrays/493-ReversePairs.cpp
+++ Arrays/493-ReversePairs.cpp
@@ -2,15 +2,50 @@
 using namespace std;
 
 class Solution {
-public:
-    int reversePairs(vector<int>& nums) {
-        int n = nums.size();
-        int cnt=0;
-        for(int i = 0; i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]>2LL*nums[j]) cnt++;
+    int mergeAndCount(vector<int>& nums, int low, int mid, int high) {
+        int cnt = 0;
+        int j = mid + 1;
+        for (int i = low; i <= mid; i++) {
+            while (j <= high && nums[i] > 2LL * nums[j]) {
+                j++;
+            }
+            cnt += (j - (mid + 1));
+        }
+        vector<int> temp;
+        int left = low, right = mid + 1;
+        
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left++]);
+            } else {
+                temp.push_back(nums[right++]);
             }
         }
+        
+        while (left <= mid) temp.push_back(nums[left++]);
+        while (right <= high) temp.push_back(nums[right++]);
+
+        for (int i = low; i <= high; i++) {
+            nums[i] = temp[i - low];
+        }
+
         return cnt;
+    }
+
+    int mergeSort(vector<int>& nums, int low, int high) {
+        if (low >= high) return 0;
+        
+        int mid = low + (high - low) / 2;
+        int cnt = mergeSort(nums, low, mid);
+        cnt += mergeSort(nums, mid + 1, high);
+        cnt += mergeAndCount(nums, low, mid, high);
+        
+        return cnt;
+    }
+
+public:
+    int reversePairs(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        return mergeSort(nums, 0, nums.size() - 1);
     }
 };
