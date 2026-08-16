71160c6ab8b6c868a006fc104edadd6e9ffa5db4
Gursimar Singh <gursimar1731@gmail.com>
2026-08-16 22:50:09 +0530

    two pointer

--- Arrays/15-ThreeSum.cpp
+++ Arrays/15-ThreeSum.cpp
@@ -4,20 +4,25 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> st;
         int n = nums.size();
-        for(int i =0;i<n;i++){
-            for(int j= i+1;j<n;j++){
-                for(int k = j+1;k<n;k++){
-                    if(nums[i]+nums[j]+nums[k]==0){
-                        vector<int> temp={nums[i],nums[j],nums[k]};
-                        sort(temp.begin(),temp.end());
-                        st.insert(temp);
-                    }
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        for(int i =0;i < n; i++){
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum = nums[i]+nums[left]+nums[right];
+                if(sum == 0){
+                    result.push_back({nums[i],nums[left],nums[right]});
+                    left++; right--;
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
                 }
+                else if(sum>0) right--;
+                else if (sum<0) left++;
             }
         }
-        vector<vector<int>> result(st.begin(), st.end());
         return result;
     }
 };
