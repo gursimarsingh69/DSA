47fb73df10d7262909883ab25c7cf34df378d5d9
Gursimar Singh <gursimar1731@gmail.com>
2026-08-16 22:37:36 +0530

    brute force

---
+++ Arrays/15-ThreeSum.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        set<vector<int>> st;
+        int n = nums.size();
+        for(int i =0;i<n;i++){
+            for(int j= i+1;j<n;j++){
+                for(int k = j+1;k<n;k++){
+                    if(nums[i]+nums[j]+nums[k]==0){
+                        vector<int> temp={nums[i],nums[j],nums[k]};
+                        sort(temp.begin(),temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+        vector<vector<int>> result(st.begin(), st.end());
+        return result;
+    }
+};
