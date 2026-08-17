ef68b2ab9599b2d9728aca2ddcf87006f1de4f25
Gursimar Singh <gursimar1731@gmail.com>
2026-08-17 22:47:58 +0530

    two pointer

--- Arrays/18-FourSum.cpp
+++ Arrays/18-FourSum.cpp
@@ -3,23 +3,33 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& arr, int target) {
-        int n = arr.size();
-        set<vector<int>> st;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    for (int l = k + 1; l < n; l++) {
-                        long long sum = (long long)arr[i] + arr[j] + arr[k] + arr[l];
-                        if (sum == target) {
-                            vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
-                            sort(temp.begin(), temp.end());
-                            st.insert(temp);
-                        }
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        if (n < 4) return ans;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                int left = j + 1;
+                int right = n - 1;
+                while (left < right) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[left], nums[right]});
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+                        left++;
+                        right--;
+                    } else if (sum < target) {
+                        left++;
+                    } else {
+                        right--;
                     }
                 }
             }
         }
-        return vector<vector<int>>(st.begin(), st.end());
+        return ans;
     }
 };
