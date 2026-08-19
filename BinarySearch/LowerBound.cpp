64ddc72e64734d0fdbfaed0d761edb73ec2e3b39
Gursimar Singh <gursimar1731@gmail.com>
2026-08-19 21:42:28 +0530

    Upper Bound in array

---
+++ BinarySearch/LowerBound.cpp
@@ -0,0 +1,23 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int target) {
+        int start = 0;
+        int end = arr.size() -1;
+        int res = arr.size();
+        while(start<=end){
+            int mid = start + (end -start)/2;
+            if(arr[mid] >= target){
+                res = mid;
+                end = mid - 1;
+            }
+            else {
+                start = mid + 1;
+            }
+        }
+        return res;
+    }
+};
